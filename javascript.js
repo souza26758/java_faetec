b2a714dd7a7369c2906579d9c22ec4e4949447a7
Davi Augusto <dssblackmusicteen@gmail.com>
2025-11-12 20:10:33 -0300

    function

---
+++ javascript.js
@@ -0,0 +1,19 @@
+function abrir_a_porta_do_carro() {
+
+    console.log("a porta está aberta");
+}
+
+function ligar_a_ignicao() {
+
+    console.log("o carro está ligado")
+}
+
+function passar_a_marcha() {
+
+    console.log("o carro está engrenado")
+}
+
+function acelerar() {
+
+    console.log("o carro está em movimento")
+}
